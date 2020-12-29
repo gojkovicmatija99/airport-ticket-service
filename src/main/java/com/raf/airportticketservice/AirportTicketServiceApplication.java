47ab74cca80dca2297d7e3ac07d7e6180e7134e6
Lazar Miladinovic <lmiladinovic2318rn@raf.rs>
2020-12-29 17:46:17 +0100

    Initial commit

---
+++ src/main/java/com/raf/airportticketservice/AirportTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.raf.airportticketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportTicketServiceApplication.class, args);
+	}
+
+}
