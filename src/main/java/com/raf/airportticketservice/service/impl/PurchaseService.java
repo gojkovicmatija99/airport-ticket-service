3267a340d2727b5977ed06064c382f6ae03e1386
gojkovicmatija99 <gojkovicmatija99@gmail.com>
2021-01-03 22:53:04 +0100

    TicketServis kontaktira UserServis da bih korisnicima poslao mejl o otkazanom letu

--- src/main/java/com/raf/airportticketservice/service/impl/PurchaseService.java
+++ src/main/java/com/raf/airportticketservice/service/impl/PurchaseService.java
@@ -3,13 +3,22 @@ package com.raf.airportticketservice.service.impl;
 import com.raf.airportticketservice.domain.Purchase;
 import com.raf.airportticketservice.repository.PurchaseRepository;
 import com.raf.airportticketservice.service.IPurchaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.jms.Queue;
 import java.util.List;
 
 @Service
 public class PurchaseService implements IPurchaseService {
     private PurchaseRepository purchaseRepository;
+
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Autowired
+    Queue usersQueue;
 
     public PurchaseService(PurchaseRepository purchaseRepository) {
         this.purchaseRepository = purchaseRepository;
@@ -28,6 +37,7 @@ public class PurchaseService implements IPurchaseService {
             return true;
         for(Purchase current:canceled) {
             //TODO: Svakom korisniku poslati mejl, oduzeti milje
+            jmsTemplate.convertAndSend(usersQueue, current.getUserId().toString());
         }
         return true;
     }
